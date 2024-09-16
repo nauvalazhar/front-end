50d7104afcdabf3714c7d5329574f7a6d2e2266c
Nauval <nauvalazhar2@gmail.com>
2024-09-16 08:02:50 +0700

    feat: offline

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
 import nextra from 'nextra';
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: 'sw.ts',
+  swDest: 'public/sw.js',
+});
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 });
 
-export default withNextra({
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-});
+export default withSerwist(
+  withNextra({
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  }),
+);
